c0a0f3f4716681a6ce412f279d9b0d33f0de4d64
baizhi958216 <1475289190@qq.com>
2022-08-05 18:18:54 +0800

    vue: add reactive
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- <vBase></vBase> -->
   <!-- <vDomDiff></vDomDiff> -->
-  <vRef></vRef>
+  <!-- <vRef></vRef> -->
+  <vReactive></vReactive>
 </template>
 
 <script setup lang="ts">
 // import vBase from './components/Vue基础.vue'
 // import vDomDiff from './components/虚拟DOM和VueKeyDiff算法.vue'
-import vRef from './components/Ref全家桶.vue'
+// import vRef from './components/Ref全家桶.vue'
+import vReactive from './components/Reactive全家桶.vue'
 </script>
 
 <style scoped>
