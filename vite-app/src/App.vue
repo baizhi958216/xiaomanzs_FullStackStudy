d299a8f68144e9aa5355c01f479c90596abfa1ac
baizhi958216 <1475289190@qq.com>
2022-08-05 17:19:30 +0800

    vue: add vue virtual dom and diff
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <vBase></vBase>
+  <!-- <vBase></vBase> -->
+  <vDomDiff></vDomDiff>
 </template>
 
 <script setup lang="ts">
-import vBase from './components/Vue基础.vue'
+// import vBase from './components/Vue基础.vue'
+import vDomDiff from './components/虚拟DOM和VueKeyDiff算法.vue'
 </script>
 
 <style scoped>
