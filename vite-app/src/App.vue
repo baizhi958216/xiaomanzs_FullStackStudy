362c4f280ad582a0445ab79ed84d5cdbafd280fd
baizhi958216 <1475289190@qq.com>
2022-08-07 10:56:05 +0800

    vue: add toReftoRefstoRaw and computed
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -2,14 +2,19 @@
   <!-- <vBase></vBase> -->
   <!-- <vDomDiff></vDomDiff> -->
   <!-- <vRef></vRef> -->
-  <vReactive></vReactive>
+  <!-- <vReactive></vReactive> -->
+  <!-- <vtoRefs></vtoRefs> -->
+  <vComputed></vComputed>
 </template>
 
 <script setup lang="ts">
 // import vBase from './components/Vue基础.vue'
 // import vDomDiff from './components/虚拟DOM和VueKeyDiff算法.vue'
 // import vRef from './components/Ref全家桶.vue'
-import vReactive from './components/Reactive全家桶.vue'
+// import vReactive from './components/Reactive全家桶.vue'
+// import vtoRefs from './components/toRef_toRefs_toRaw.vue'
+import vComputed from './components/Vue计算属性.vue'
+
 </script>
 
 <style scoped>
