953dd2fb45c5248696670f3e4a43ab5cd2956fc4
baizhi958216 <1475289190@qq.com>
2022-08-07 15:46:00 +0800

    vue: add parent % child components prarm
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vite-app/src/layout/Menu/index.vue
+++ vite-app/src/layout/Menu/index.vue
@@ -1,14 +1,53 @@
 <template>
   <div class="menu">
-    菜单区域
+    父组件的·{{ title }}·
+    <br>
+    父组件的·{{ data }}·
+    <hr>
+    <button @click="clickTap1">派发</button>
+    <hr>
+    <button @click="clickTap2">派发</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
+
+// 接收父组件参数
+type Props = {
+  title?: string,
+  data?: number[]
+}
+// defineProps<Props>()
+
+// 包含默认(父组件可以不传值)
+// 如果父组件无值传入, 使用默认值
+withDefaults(defineProps<Props>(),{
+  title:'父组件没传值过来',
+  data:()=>[1,2,3,3,2]
+})
+
+// 向父组件传值
+const list = reactive<number[]>([5,5,5,5,5])
+const bool1 = false
+const bool2 = true
+const emit = defineEmits(['on-sssclick','on-cccclick'])
+const clickTap1 = () => {
+  emit('on-sssclick',list,bool1)
+}
+const clickTap2 = () => {
+  emit('on-cccclick',bool2)
+}
+
+// 向父组件暴露 
+defineExpose({
+  list,
+  bool2
+})
 </script>
 
 <style scoped lang="less">
-.menu{
+.menu {
   width: 200px;
   border-right: 1px solid #CCC;
 }
--- vite-app/src/layout/index.vue
+++ vite-app/src/layout/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="layout">
-      <Menu></Menu>
+    <!-- 
+      自定义事件  @on-sssclick @on-cccclick
+
+      接收子组件暴露的参数  ref="menus"
+    -->
+    <Menu ref="menus" :data="list" title="菜单" @on-sssclick="getList" @on-cccclick="getBool"></Menu>
     <div class="layout-right">
       <Header></Header>
       <Content></Content>
@@ -12,15 +17,30 @@
 import Menu from './Menu/index.vue'
 import Header from './Header/index.vue'
 import Content from './Content/index.vue'
+import { reactive,ref } from 'vue';
+const list = reactive<number[]>([1, 2, 3, 4, 5])
+
+// 接受子组件参数
+const getList = (list: number,bool1:boolean) => {
+  console.log(list, '====>子组件的List',bool1,'=====>子组件的bool1')
+}
+// 接受子组件参数
+// 接收子组件暴露的参数
+const menus=ref(null)
+const getBool = (bo:boolean) => {
+  console.log(bo,'=====>子组件的bool2')
+  console.log(menus.value);
+}
 </script>
 
 <!-- scoped作用在元素添加一个data-v-xxxxxxx的id选择器 -->
 <style scoped lang="less">
-.layout{
+.layout {
   display: flex;
   height: 100vh;
   overflow: hidden;
-  &-right{
+
+  &-right {
     flex: 1;
     display: flex;
     flex-direction: column;
